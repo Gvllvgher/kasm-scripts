884275b6a3c7a6c5367f68d7f20f30912efea11b
Justin <justingallagher@outlook.com>
2026-08-25 10:03:01 -0400

    Add installation script for Fedora setup

---
+++ fedora/install.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+# Defines the current script directory
+SCRIPT_DIR="$( cd -- "$( dirname -- "${BASH_SOURCE[0]:-$0}";     )" &> /dev/null && pwd 2> /dev/null;     )";
+
+# Set Theme
+THEME="Greybird-dark"
+xfconf-query -c xsettings -p /Net/ThemeName -s $THEME
+xfconf-query -c xfwm4 -p /general/theme -s $THEME
+gsettings set org.gnome.desktop.interface gtk-theme $THEME
+
+# Set Dark Mode
+gsettings set org.gnome.desktop.interface color-scheme 'prefer-dark'
+
+# Set Wallpaper
+IMAGE_FILE="/usr/share/backgrounds/bg_kasm.png"
+SCREEN_PATH="/backdrop/screen0/monitorVNC-0/workspace0/last-image"
+# Get screen path using 'xfconf-query -c xfce4-desktop -mv' and then change background
+xfconf-query -c xfce4-desktop -p $SCREEN_PATH -s $IMAGE_FILE
+
+# Download Bitwarden
+BITWARDEN_URL="https://bitwarden.com/download/?app=desktop&platform=linux&variant=rpm"
+BITWARDEN_PATH="~/bitwarden.rpm"
+curl -L -o $BITWARDEN_PATH $BITWARDEN_URL
+sudo dnf install -y $BITWARDEN_PATH
+rm $BITWARDEN_PATH
+
+# Install Apps
+sudo dnf install -y firefox \
+    neovim \
+    bind-utils \
+    iputils \
+    $BITWARDEN_PATH
+
+# Run configuration script(s)
+$SCRIPT_DIR/../.config/firefox/configure-firefox.sh
