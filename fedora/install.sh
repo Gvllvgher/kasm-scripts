1c1abdbae93487faf17eb20a23cc0c123645b310
Justin <justingallagher@outlook.com>
2026-08-26 09:38:48 -0400

    Fix path for zsh shell in install script

--- fedora/install.sh
+++ fedora/install.sh
@@ -19,7 +19,7 @@ SCREEN_PATH="/backdrop/screen0/monitorVNC-0/workspace0/last-image"
 xfconf-query -c xfce4-desktop -p $SCREEN_PATH -s $IMAGE_FILE
 
 # Set shell
-sudo chsh kasm-user -s /usr/bin/zsh
+sudo chsh kasm-user -s /usr/sbin/zsh
 
 # Run configuration script(s)
 $SCRIPT_DIR/../.config/firefox/configure-firefox.sh
